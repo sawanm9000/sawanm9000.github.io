fcdecaf0c94f2bc8cadc78bc3e9359a4a1a94fbb
utopianknight <thesithlord2007+github@gmail.com>
2021-09-16 22:48:20 +0400

    Minor changes to contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,23 +4,23 @@
       <form name="contact-form" action="https://kwesforms.com/api/foreign/forms/90oV3Vjk6J261C8jDRU7" class="grid grid-cols-1 gap-y-4 max-w-md w-full content-start px-3 sm:px-0 flex-grow bg-gray-100 pb-24 kwes-form" has-recaptcha-v2>
         <label class="block">
           <span>Name<span class="text-red-600">*</span></span>
-          <input id="name" type="text" name="name" class="mt-1 block w-full border " rules="required|max:255">
+          <input id="name" type="text" name="name" class="mt-1 block w-full border" rules="required|min:5|max:255">
         </label>
 
         <label class="block">
           <span>Email address<span class="text-red-600">*</span></span>
-          <input id="email" type="email" name="email" class="mt-1 block w-full" rules="required|max:255|email">
+          <input id="email" type="email" name="email" class="mt-1 block w-full" rules="required|email">
         </label>
 
 
         <label class="inline-flex items-center">
-          <input v-model="cvRequest" name="request_cv" type="checkbox" class="border-gray-600 border text-black focus:border-gray-300 focus:ring-black">
+          <input id="checked" v-model="cvRequest" name="request_cv" value="checked" label="checked" type="checkbox" class="border-gray-600 border text-black focus:border-gray-300 focus:ring-black">
           <span class="ml-2">Request CV/resume</span>
         </label>
 
         <label :class="cvRequest ? 'block' : 'hidden'">
           <span>Company name<span class="text-red-600">*</span></span>
-          <input id="company" type="text" name="company_name" class="mt-1 block w-full" :rules="cvRequest ? 'required|max:255' : ''">
+          <input id="company" type="text" name="company_name" class="mt-1 block w-full" rules="max:255">
         </label>
 
         <label :class="cvRequest ? 'block' : 'hidden'">
@@ -34,11 +34,11 @@
         </label>
 
         <label class="block">
-          <span>Are you human?<span class="text-red-600">*</span></span>
+          <span>Are you a robot?<span class="text-red-600">*</span></span>
           <div class="g-recaptcha mx-auto mt-1" data-sitekey="6LfwaXAcAAAAAIRTID-LFsPM6-CiJuOq5vDrWneb"></div>
         </label>
 
-        <button type="submit" value="Send" class="bg-yellow-500 hover:bg-yellow-400 px-5 py-2.5 rounded-sm uppercase text-sm font-bold text-gray-900 hover:text-black border-t border-yellow-300 shadow-md tracking-wide w-max justify-self-start cursor-pointer">Send</button>
+        <button type="submit" value="Send" class="bg-yellow-500 hover:bg-yellow-400 px-5 py-2.5 rounded-sm uppercase text-sm font-bold text-gray-900 hover:text-black border-t border-yellow-300 shadow-md tracking-wide w-max cursor-pointer mt-4">Send</button>
       </form>
 
       <Footer />
@@ -58,7 +58,6 @@ export default {
   head() {
     return {
       script: [{
-        // src: "https://www.google.com/recaptcha/api.js?render=6LeaOnAcAAAAADtzMyTsevduxEF2qHB-Sv2uzmGP"
         src: "https://www.google.com/recaptcha/api.js",
         defer: true
       }]
