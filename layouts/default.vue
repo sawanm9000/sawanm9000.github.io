9fffd239084f0517105aa71728ac2f8c3a6e6dde
utopianknight <thesithlord2007+github@gmail.com>
2021-09-17 21:15:08 +0400

    Minor changes to drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
   <div class="relative min-h-screen pt-20 bg-gray-100">
     <header class="h-16 absolute top-0 w-full sm:w-auto sm:inset-x-10 md:inset-x-28 lg:inset-x-40 flex flex-col sm:flex-row justify-center sm:justify-between items-center max-w-screen-lg sm:px-5 z-10 mx-auto" :class="navBarLight ? 'text-black' : 'text-white'">
       <!-- <div>SaM</div> -->
-      <NuxtLink to="/">
-        <img src="/logo.png" alt="Logo" style="height: 32px" class="filter" :class="navBarLight ? 'invert' : 'invert-0'">
+      <NuxtLink to="/" :class="drawer ? 'fixed z-10' : 'static'">
+        <img src="/logo.png" alt="Logo" style="height: 32px" class="filter" :class="drawer ? '' : navBarLight ? 'invert' : 'invert-0'">
       </NuxtLink>
 
       <div class="hidden sm:flex gap-x-4 text-lg">
@@ -12,17 +12,19 @@
         <NuxtLink to="/contact" class="border px-3 py-1.5 rounded-sm border-opacity-0 hover:border-opacity-100 w-max" :class="navBarLight ? 'border-black' : 'border-white'">Contact</NuxtLink>
       </div>
 
-      <div v-if="drawer" class="fixed top-0 h-screen bg-gray-900 w-full flex flex-col gap-y-4 justify-center items-center text-xl font-thin tracking-wider">
+      <div v-if="drawer" class="fixed top-0 h-screen bg-gray-900 w-full flex flex-col gap-y-4 justify-center items-center text-xl font-light tracking-wider z-0">
         <NuxtLink to="/" class="border px-3 py-1.5 text-white border-white rounded-sm border-opacity-0 hover:border-opacity-100 w-max" :class="navBarLight ? 'border-black' : 'border-white'">Home</NuxtLink>
         <NuxtLink to="/blog" class="border px-3 py-1.5 text-white border-white rounded-sm border-opacity-0 hover:border-opacity-100 w-max" :class="navBarLight ? 'border-black' : 'border-white'">Blog</NuxtLink>
         <NuxtLink to="/contact" class="border px-3 py-1.5 text-white border-white rounded-sm border-opacity-0 hover:border-opacity-100 w-max" :class="navBarLight ? 'border-black' : 'border-white'">Contact</NuxtLink>
       </div>
 
       <div class="absolute right-4 top-3 flex sm:hidden">
-        <button class="flex flex-col gap-y-2 w-10 p-2" @click="drawer = !drawer">
-          <div class="w-full h-px" :class="drawer ? 'bg-white' : navBarLight ? 'bg-black' : 'bg-white'"></div>
-          <div class="w-full h-px" :class="drawer ? 'bg-white' : navBarLight ? 'bg-black' : 'bg-white'"></div>
-          <div class="w-full h-px" :class="drawer ? 'bg-white' : navBarLight ? 'bg-black' : 'bg-white'"></div>
+        <button class="flex flex-col gap-y-2 w-10 h-10 p-2 relative" @click="drawer = !drawer">
+          <div class="w-full h-px" :class="drawer ? 'hidden' : navBarLight ? 'bg-black' : 'bg-white'"></div>
+          <div class="w-full h-px" :class="drawer ? 'hidden' : navBarLight ? 'bg-black' : 'bg-white'"></div>
+          <div class="w-full h-px" :class="drawer ? 'hidden' : navBarLight ? 'bg-black' : 'bg-white'"></div>
+          <div class="w-8 h-px transform rotate-45 bg-white absolute origin-center top-5 left-1" :class="drawer ? 'block' : 'hidden'"></div>
+          <div class="w-8 h-px transform -rotate-45 bg-white absolute origin-center top-5 left-1" :class="drawer ? 'block' : 'hidden'"></div>
         </button>
       </div>
     </header>
