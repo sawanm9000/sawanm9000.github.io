484e96e6e1486f5ba6f450ca79c7002670c8722e
utopianknight <utopianknight@users.noreply.github.com>
2021-09-17 16:35:01 +0000

    deploy: 272ff02464212680f65981e7182822a77e628d6b

---
+++ _nuxt/static/1631896499/blog/browser-concept/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631896499",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fbrowser-concept",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
