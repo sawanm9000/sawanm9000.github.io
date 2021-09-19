135d0d163f9a5c47e0f5acca319a8b0ca6fe794c
utopianknight <thesithlord2007+github@gmail.com>
2021-09-19 20:50:59 +0400

    Added error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
+  <div class="flex flex-col justify-center items-center h-screen absolute top-0 left-0 w-full">
+    <h1 v-if="error.statusCode === 404" class="text-4xl font-bold mb-8">Page not found</h1>
     <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+    <NuxtLink to="/" class="text-base text-blue-500">&#8592; Home page</NuxtLink>
+    <Footer class="fixed bottom-0 w-full" />
   </div>
 </template>
 
